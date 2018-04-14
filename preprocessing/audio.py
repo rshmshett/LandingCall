213071350c5c34bef5102d048246b230c0946ce1
sarahaguasvivas <saag5228@colorado.edu>
2018-04-14 17:45:46 -0600

    still working on real time fft

--- preprocessing/audio.py
+++ preprocessing/audio.py
@@ -2,15 +2,9 @@ import pyaudio
 import threading
 import atexit
 import numpy as np
-
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
-
-import sys
-
-import numpy as np
-import matplotlib.pyplot as plt
 from PyQt4 import QtGui, QtCore
 
 class MplFigure(object):
