9a55a7806703111a2f16ff2b4a43f5618d6cbfb5
Deepak Kyasaralli Thimmappa <deky6365@colorado.edu>
2018-05-09 13:10:29 -0600

    Api file to handle android request

---
+++ Api.php
@@ -0,0 +1,103 @@
+<?php 
+
+	function createFall($id, $event, $deviceip){
+		define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'root');
+	define('DB_NAME', 'landingfall');
+		$con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	 
+			//Checking if any error occured while connecting
+			if (mysqli_connect_errno()) {
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+	 
+			//finally returning the connection link 
+			
+		$stmt = $con->prepare("INSERT INTO falls (`id`, `event`, `deviceip`) VALUES (?, ?, ?)");
+		$stmt->bind_param("ssis", $id, $event, $deviceip);
+		if($stmt->execute())
+			echo true; 
+		echo false; 
+	}
+	function getFalls(){
+		define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'root');
+	define('DB_NAME', 'landingfall');
+		$con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	 
+			//Checking if any error occured while connecting
+			if (mysqli_connect_errno()) {
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+	 
+			//finally returning the connection link 
+			
+		$stmt = $con->prepare("SELECT id, event, deviceip FROM falls");
+		$stmt->execute();
+		$stmt->bind_result($id, $event, $deviceip);
+		
+		$heroes = array(); 
+		
+		while($stmt->fetch()){
+			$hero  = array();
+			$hero['id'] = $id; 
+			$hero['event'] = $event; 
+			$hero['deviceip'] = $deviceip; 
+			
+			array_push($heroes, $hero); 
+		}
+		
+		return $heroes; 
+	}
+	
+	$response = array();
+	
+	if(isset($_GET['apicall'])){
+		
+		switch($_GET['apicall']){
+			
+			case 'createFall':
+				
+				$result = createHero(
+					$_GET['id'],
+					$_GET['event'],
+					$_GET['deviceip'],
+					
+				);
+				
+
+				if($result){
+					$response['error'] = false; 
+
+					$response['message'] = 'Event registered successfully';
+
+					$response['falls'] = '';
+				}else{
+
+					//if record is not added that means there is an error 
+					$response['error'] = true; 
+
+					//and we have the error message
+					$response['message'] = 'Some error occurred please try again';
+				}
+				
+			break; 
+			
+			case 'getFalls':
+				$response['error'] = false; 
+				$response['message'] = 'Request successfully completed';
+				$response['falls'] = getFalls();
+			break; 
+		}
+		
+	}else{
+		//if invalid call
+		$response['error'] = true; 
+		$response['message'] = 'Invalid API Call';
+	}
+	
+	echo json_encode($response);
+?>	
+	
